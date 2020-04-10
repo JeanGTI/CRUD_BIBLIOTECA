745ac570cd20d6b1e89f8491bd73673b304d02e1
Jean Augusto <jean.aug02@gmail.com>
2020-04-10 17:26:19 -0300

    Rotas

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('autor', 'controllerAutor@visualisa_Autor');
+Route::get('autor/novo', 'controllerAutor@novo_Autor');
+Route::get('autor/{autor},/edita', 'controllerAutor@edita_Autor');
+Route::post('autor/salva', 'controllerAutor@salva_Autor');
+Route::patch('autor/{autor}', 'controllerAutor@atualiza_Autor');
+Route::delete('autor,{autor}', 'controllerAutor@deleta_Autor');
+
